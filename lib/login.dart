28fd7b78f6cca496f5da34d1e129e09d888b5d33
Nit89 <nitesh382singh@gmail.com>
2021-09-14 23:13:36 +0530

    google signin implemented

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:keep_notes/home.dart';
 import 'package:keep_notes/service/auth.dart';
+import 'package:keep_notes/service/login_info.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,6 +13,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +26,12 @@ class _LoginState extends State<Login> {
           children: [
             SignInButton(Buttons.Google, onPressed: () async {
               await signInWithGoogle();
+              final User? currentUser = await _auth.currentUser;
+              LocalDataSaver.saveLoginData(true);
+              LocalDataSaver.saveImg(currentUser!.photoURL.toString());
+              LocalDataSaver.saveMail(currentUser.email.toString());
+              LocalDataSaver.saveName(currentUser.displayName.toString());
+              //     await FireDB().getAllStoredNotes();
               Navigator.pushReplacement(
                   context, MaterialPageRoute(builder: (context) => Home()));
             })
